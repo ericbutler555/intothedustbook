9c90c10b1d7f257ed85bda4e69e2905471492538
Eric Butler <ericbutler555@gmail.com>
2021-03-20 16:33:54 -0400

    Customize footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,17 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
-    </>
-  )
+    <footer className={styles.footer}>
+      <div className="innerContainer">
+        <p>
+          All content &copy; 2021 by Jack Lyons. Site design by{" "}
+          <a className="linkUncolored" href="https://ericbutler555.github.io/resume-ng/" target="_blank">
+            Eric Butler
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
 }
