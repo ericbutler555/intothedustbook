c33176a854220a23dddae1778f231f32e8a3d7fd
Eric Butler <ericbutler555@gmail.com>
2021-03-20 16:32:57 -0400

    Customize head content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '@styles/globals.css'
+import Head from "next/head";
+import "@styles/globals.css";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Trade+Winds&display=swap" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default Application
+export default Application;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Into the Dust: The Virgin - A Burning Man Story by Jack Lyons</title>
+        <meta name="description" content="Available in paperback, hardcover and e-book, read Jack Lyons's new novel about the intense, life-changing experiences a woman has on her first trip to Burning Man." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <main style={{ flex: "1 1 100%", width: "100%" }}>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
-
       <Footer />
-    </div>
-  )
+    </>
+  );
 }
