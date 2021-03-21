ea677ffee6cbff9b8eb44ee3cd0df01ed2785ba4
Eric Butler <ericbutler555@gmail.com>
2021-03-21 04:37:24 -0400

    Fix link in header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
-import styles from './Header.module.css'
+import Link from "next/link";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.pageHeader}>
       <div className={styles.inner}>
         <span>Fuck your Burn.</span>
-        <a className={styles.button} href="#buy">
-          BUY THE BOOK
-        </a>
+        <Link href="/#buy">
+          <a className={styles.button}>BUY THE BOOK</a>
+        </Link>
       </div>
     </header>
   );
